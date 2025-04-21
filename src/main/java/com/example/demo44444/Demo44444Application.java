240196af9f062788531e71ee2ecdb7d9da3aa304
aliali <aliali@test.com>
2025-04-21 11:06:47 +0300

    first

---
+++ src/main/java/com/example/demo44444/Demo44444Application.java
@@ -0,0 +1,13 @@
+package com.example.demo44444;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo44444Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo44444Application.class, args);
+    }
+
+}
